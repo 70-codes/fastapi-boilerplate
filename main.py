11347535bdcf5d253bcc4054e74a66a75edc862c
70-codes <kinuthiasteve098@gmail.com>
2024-10-04 19:43:51 +0300

    v1

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from services import create_db
 
 app = FastAPI()
 create_db()
+
+origins = [
+    "http://localhost:5173",
+    "https://my-accountant.vercel.app",
+    "http://localhost:3000",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
